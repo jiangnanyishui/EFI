253196e8ea25cd15a29fb67b378f61b9d6ef48a2
kingsley <hryzc247yixin@163.com>
2015-09-12 17:58:05 +0800

    create

---
+++ mysql/mysql/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace mysql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string constr = "User Id=root;Host=localhost;Database=test;password=123456";
+            MySqlConnection conn = new MySqlConnection(constr);
+            conn.Open();
+            conn.Close();
+
+        }
+    }
+}
