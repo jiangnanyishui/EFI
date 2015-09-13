b975f87c08d4f16750474616c1b83103d79a0d87
kingsley <hryzc247yixin@163.com>
2015-09-13 10:08:15 +0800

    mysql读取数据!

--- mysql/mysql/Program.cs
+++ mysql/mysql/Program.cs
@@ -11,9 +11,12 @@ namespace mysql
     {
         static void Main(string[] args)
         {
-            string constr = "User Id=root;Host=localhost;Database=test;password=123456";
+            string constr = "User Id=root;Host=localhost;Database=world;password=123456";
             MySqlConnection conn = new MySqlConnection(constr);
             conn.Open();
+            MySqlCommand mycm = new MySqlCommand("select * from city", conn);
+            MySqlDataReader mysdr = mycm.ExecuteReader();
+            Console.ReadKey();
             conn.Close();
 
         }
